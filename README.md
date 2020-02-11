14c951b83342dd7fd9aa13772248ba6321f41e3e
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2020-02-12 00:20:57 +0200

    upd

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ![Not working yet :'(](https://img.shields.io/badge/-Not%20working%20yet%20%3A%27%28-red)
 
 A plugin to instruct Bloop to compile files on Tmpfs to spare some SSD cycles.
+
+## TODO
+
+- Add a root project name in-between .bloop/ & normalizedName
+- Add instruction
+- Link to the original issue:  
+  https://github.com/scalacenter/bloop/issues/1031
+- Publish to Maven
--- src/main/scala/bloop/integrations/sbt/SbtBloopTmpfs.scala
+++ src/main/scala/bloop/integrations/sbt/SbtBloopTmpfs.scala
@@ -9,9 +9,11 @@ object BloopTmpfsPlugin extends AutoPlugin {
   override def requires = BloopPlugin
   override def trigger = allRequirements
 
+  private val tmpDir = sys.env.get("TMPDIR").getOrElse("/tmp")
+
   override lazy val projectSettings = Seq(
-    Compile / bloopClassDirectory := file(s"/tmp/bloop-project/${ normalizedName.value }"),
-    Compile / bloopTargetDir := file(s"/tmp/bloop-project/${ normalizedName.value }"),
-    Compile / bloopAnalysisOut := Some(file(s"/tmp/bloop-project/${ normalizedName.value }"))
+    Compile / bloopClassDirectory := file(s"$tmpDir/.bloop/${ normalizedName.value }"),
+    Compile / bloopTargetDir := file(s"$tmpDir/.bloop/${ normalizedName.value }"),
+    Compile / bloopAnalysisOut := Some(file(s"$tmpDir/.bloop/${ normalizedName.value }"))
   )
 }
